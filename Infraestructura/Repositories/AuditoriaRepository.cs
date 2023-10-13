0475164d1d7f486ff7a9a00ea099e8b43776ab1b
marianita13 <marianitacero.1913@gmail.com>
2023-10-12 22:30:38 -0500

    feat: :art: Unit Of Work

---
+++ Infraestructura/Repositories/AuditoriaRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infraestructura.Data;
+
+namespace Infraestructura.Repositories
+{
+    // public class AuditoriaRepository : GenericRepository<Auditoria>, IAuditoria
+    // {
+    //     private readonly NotiAppContext _context;
+    //     public AuditoriaRepository(NotiAppContext context) : base(context)
+    //     {
+    //         _context = context;
+    //     }
+    // }
+}
--- Infraestructura/UnitOfWork/UnitOfWork.cs
+++ Infraestructura/UnitOfWork/UnitOfWork.cs
@@ -2,11 +2,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infraestructura.Data;
+using Infraestructura.Repositories;
 
 namespace Infraestructura.UnitOfWork
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork, IDisposable
     {
-        
+        private readonly NotiAppContext _context;
+        public UnitOfWork(NotiAppContext context)
+        {
+            _context = context;
+        }
+
+        public IAuditoria _Auditorias;
+        public IBlockChain _BlockChains;
+        public IEstadoNotificacion _EstadosNots;
+        public IFormato _Formatos;
+        public IGenericosvsSubModulos _GenericosVSSubModulos;
+        public IHiloRespuestaNot _HilosRespuestas;
+        public IMaestrosvsSubModulos _MaestrosVSSubModulos;
+        public IModuloNotificaciones _ModuloNotificaciones;
+        public IPermisosGenericos _PermisosGenericos;
+        public IRadicados _Radicados;
+        public IRol _Roles;
+        public IRolvsMaestro _RolesVSMaestros;
+        public ISubModulo _SubModulos;
+        public ITipoNotificaciones _TiposNost;
+        public ITipoRequerimiento _TipoRequerimientos;
+
+        // public IAuditoria Auditoria{
+        //     get{
+        //         if (_Auditorias == null){
+        //             _Auditorias = new AuditoriaRepository(_context);
+        //         }
+        //         return _Auditorias;
+        //     }
+        // }
+
+
+        public Task<int> SaveAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
